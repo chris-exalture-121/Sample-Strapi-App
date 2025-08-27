4bf56d77e942f7ee4cd229a0fe42db5595ea951c
Chris <chrisj@exalture.com>
2025-08-27 09:01:56 +0530

    New content tyoe added

---
+++ src/api/booking-window/controllers/booking-window.ts
@@ -0,0 +1,7 @@
+/**
+ * booking-window controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::booking-window.booking-window');
---
+++ src/api/booking-window/routes/booking-window.ts
@@ -0,0 +1,7 @@
+/**
+ * booking-window router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::booking-window.booking-window');
