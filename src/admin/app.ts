26841414639ec61a78c52d2c80c0098448915b03
Chris <chrisj@exalture.com>
2025-12-06 01:26:12 +0530

    added icons

--- src/admin/app.ts
+++ src/admin/app.ts
@@ -1,3 +1,5 @@
+import { Store, User, ShoppingCart } from "lucide-react";
+
 export default {
   config: {
     // your config (logo, theme, etc)
@@ -7,7 +9,7 @@ export default {
     // Kitchens link
     app.addMenuLink({
       to: "/kitchens",
-      icon: "Store", // built-in icon
+      icon: Store, // built-in icon
       intlLabel: {
         id: "kitchens.page.title",
         defaultMessage: "Kitchens",
@@ -22,7 +24,7 @@ export default {
     // Users link
     app.addMenuLink({
       to: "/users",
-      icon: "User", // built-in icon
+      icon: User, // built-in icon
       intlLabel: {
         id: "users.page.title",
         defaultMessage: "Users",
@@ -35,7 +37,7 @@ export default {
     });
     app.addMenuLink({
       to: "/orders",
-      icon: "Order", // built-in icon
+      icon: ShoppingCart, // built-in icon
       intlLabel: {
         id: "orders.page.title",
         defaultMessage: "Orders",
